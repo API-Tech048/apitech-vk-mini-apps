aca60efe18122040cc3c163c77d438e72fc67c92
API-Tech048 <62449433+API-Tech048@users.noreply.github.com>
2020-03-21 02:07:47 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import 'core-js/features/map';
 import 'core-js/features/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import connect from '@vkontakte/vk-connect';
+import bridge from '@vkontakte/vk-bridge'; 
 import App from './App';
 import * as eruda from 'eruda';
 import * as erudaCode from 'eruda-code';
 import * as erudaDom from 'eruda-dom';
 
 // Init VK  Mini App
-connect.send('VKWebAppInit');
+bridge.send('VKWebAppInit');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 if (process.env.NODE_ENV === 'development') {
