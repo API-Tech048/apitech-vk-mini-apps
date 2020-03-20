3515e3e7f915925caad627b008b8e8e571569a68
API-Tech048 <62449433+API-Tech048@users.noreply.github.com>
2020-03-21 02:39:10 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'core-js/features/map';
 import 'core-js/features/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import bridge from '@vkontakte/vk-bridge'; 
+import bridge from '@vkontakte/vk-bridge';
 import App from './App';
 import * as eruda from 'eruda';
 import * as erudaCode from 'eruda-code';
